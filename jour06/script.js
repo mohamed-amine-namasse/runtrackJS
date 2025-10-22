8bbffea3bd22024ca03d5f257a12c5297c3e0239
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-10-22 18:22:48 +0200

    commit

---
+++ jour06/script.js
@@ -0,0 +1,37 @@
+function rebootMonde() {
+  // Citations aléatoires du film "Blade Runner" (1982)
+  const citations = [
+    {
+      titre: "Blade Runner",
+      texte:
+        "<p>I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die.</p><p class='fw-bold'>— Roy Batty</p>",
+    },
+    {
+      titre: "Blade Runner",
+      texte:
+        "<p>It's too bad she won't live! But then again, who does?</p><p class='fw-bold'>— Gaff</p>",
+    },
+    {
+      titre: "Blade Runner",
+      texte:
+        "<p>If only you could see what I've seen with your eyes.</p><p class='fw-bold'>— Roy Batty</p>",
+    },
+    {
+      titre: "Blade Runner",
+      texte:
+        "<p>I don't know why he saved my life. Maybe in those last moments, he loved life more than he ever had before. Not just his life, anybody's life, my life. All he'd wanted were the same answers the rest of us want. Where did I come from? Where am I going? How long have I got? All I could do was sit there and watch him die.</p><p class='fw-bold'>— Rick Deckard</p>",
+    },
+  ];
+
+  // Sélectionne une citation aléatoire
+  const citationAleatoire =
+    citations[Math.floor(Math.random() * citations.length)];
+
+  // Met à jour le titre (h2)
+  const titreElement = document.getElementById("jumbotron-title");
+  titreElement.textContent = citationAleatoire.titre;
+
+  // Met à jour le contenu (div#jumbotron-content)
+  const contenuElement = document.getElementById("jumbotron-content");
+  contenuElement.innerHTML = citationAleatoire.texte;
+}
